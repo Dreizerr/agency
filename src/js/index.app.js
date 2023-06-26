aa6f18ffd3395aa696b2d74a340e9588bf9c6914
Dreizer <artem39r@mail.ru>
2023-06-26 23:37:37 +0200

    callback, benefit-blocks, our-clients

--- src/js/index.app.js
+++ src/js/index.app.js
@@ -1,6 +1,7 @@
 import * as utils from "./modules/utils.js";
 import * as header from "./modules/header.js";
 import * as tabs from "./modules/tabs.js";
+import * as masks from "./modules/masks.js";
 
 import { useDynamicAdapt } from "./libs/dynamicAdapt.js";
 
--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -4,38 +4,41 @@ import * as utils from "../modules/utils.js";
 
 if (allTabs.length > 0) {
   allTabs.forEach((tabs) => {
+    if (tabs.classList.contains("tabs--mobile-disabled") && window.innerWidth < 650) {
+      return false;
+    }
     const blockLinks = tabs.querySelector(`.tabs-links`);
     const blockBodys = tabs.querySelector(`.tabs-bodys`);
 
     tabs.addEventListener("click", (e) => {
-      const targetElement = e.target;
-
-      if (targetElement.closest(`[data-tab-link]`)) {
-        const tab = targetElement.dataset.tabLink;
-        const activeLink = blockLinks.querySelector(`.active`);
-        const activeBody = blockBodys.querySelector(`.active`);
-
-        if (targetElement.classList.contains("active")) {
-          return false;
-        }
-
-        if (blockBodys.querySelector(`[data-tab-body="${tab}"]`)) {
-          const tabBody = blockBodys.querySelector(`[data-tab-body="${tab}"]`);
-          targetElement.classList.add("active");
-          tabBody.classList.add("active");
-        } else {
-          return false;
-        }
-
-        if (activeLink || activeBody) {
-          activeLink.classList.remove("active");
-          activeBody.classList.remove("active");
-        }
-      }
+      tabsHandler(e, blockLinks, blockBodys);
     });
   });
 }
 
-// if (utils.isMobile.any()) {
-//   const tabsLinksMobileDisables = document.querySelectorAll(`.tabs--mobile-disables`);
-// }
+function tabsHandler(e, blockLinks, blockBodys) {
+  const targetElement = e.target;
+
+  if (targetElement.closest(`[data-tab-link]`)) {
+    const tab = targetElement.dataset.tabLink;
+    const activeLink = blockLinks.querySelector(`.active`);
+    const activeBody = blockBodys.querySelector(`.active`);
+
+    if (targetElement.classList.contains("active")) {
+      return false;
+    }
+
+    if (blockBodys.querySelector(`[data-tab-body="${tab}"]`)) {
+      const tabBody = blockBodys.querySelector(`[data-tab-body="${tab}"]`);
+      targetElement.classList.add("active");
+      tabBody.classList.add("active");
+    } else {
+      return false;
+    }
+
+    if (activeLink || activeBody) {
+      activeLink.classList.remove("active");
+      activeBody.classList.remove("active");
+    }
+  }
+}
