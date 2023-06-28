6537679481bdd0d6f0edef05aa22f52553cc96f5
Dreizer <artem39r@mail.ru>
2023-06-29 00:20:50 +0200

    update

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,10 +1,18 @@
 const header = document.querySelector(`.header`);
 
-header.addEventListener("click", (e) => {
-  let targetElement = e.target;
+if (header) {
+  header.addEventListener("click", (e) => {
+    let targetElement = e.target;
 
-  if (targetElement.closest(`.header-nav__submenu-toggler`) && !targetElement.closest(`.header-nav__submenu`)) {
-    const subMenu = targetElement.closest(`.header-nav__submenu-toggler`).querySelector(`.header-nav__submenu`);
-    subMenu.classList.toggle("active");
-  }
-});
+    if (targetElement.closest(`.header-nav__submenu-toggler`) && !targetElement.closest(`.header-nav__submenu`)) {
+      const subMenu = targetElement.closest(`.header-nav__submenu-toggler`).querySelector(`.header-nav__submenu`);
+      subMenu.classList.toggle("active");
+    }
+  });
+  const allNavSubMenu = header.querySelectorAll(`.header-nav__submenu`);
+  allNavSubMenu.forEach((subMenu) => {
+    subMenu.addEventListener("mouseleave", (e) => {
+      e.target.classList.remove("active");
+    });
+  });
+}
