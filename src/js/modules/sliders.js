344711db5a4afddd52bc6ee933a51304cf1a5f21
Dreizer <artem39r@mail.ru>
2023-07-01 13:21:19 +0200

    cases, info-cards, bid

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,5 +1,31 @@
-import Swiper from "swiper";
+import Swiper, { Navigation } from "swiper";
 
-if (document.querySelector(`.`)) {
-  const introSwiper = new Swiper(".", {});
+if (document.querySelector(`.cases-swiper`)) {
+  const casesSwiper = new Swiper(".cases-swiper", {
+    slidesPerView: 3,
+
+    modules: [Navigation],
+
+    spaceBetween: 35,
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      900: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+      1301: {
+        slidesPerView: 3,
+      },
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  console.log(casesSwiper);
 }
--- src/js/yandex-direct.app.js
+++ src/js/yandex-direct.app.js
@@ -3,6 +3,7 @@ import * as headerBlock from "./modules/header.js";
 import * as masks from "./modules/masks.js";
 import * as spollers from "./modules/spollers.js";
 import * as burger from "./modules/burger.js";
+import * as sliders from "./modules/sliders.js";
 
 import { useDynamicAdapt } from "./libs/dynamicAdapt.js";
 
